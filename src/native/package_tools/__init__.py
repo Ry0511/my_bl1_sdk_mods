0ca7cb38af713ee4826b3690fb3a050dc36bd164
Ryan Thomas Payne <50511769+Ry0511@users.noreply.github.com>
2026-05-30 21:17:17 +0100

    remove abandoned projects

--- src/native/package_tools/__init__.py
+++
@@ -1,4 +0,0 @@
-from mods_base import build_mod
-from .package_tools import *
-
-build_mod()
--- src/native/package_tools/editor.py
+++
@@ -1,6 +0,0 @@
-
-from package_tools import get_launch_args
-
-def launched_as_editor() -> bool:
-    args = get_launch_args()
-    return args is not None and "-editor" in args.lower().split()
