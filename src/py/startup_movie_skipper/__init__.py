7d2d1b5fb3e561bcfee2de85541b97be757f66f7
-Ry <ryanpayne0511@gmail.com>
2026-06-28 00:29:56 +0100

    remove outdated files and repackage startup_movie_skipper for v1.3

--- src/py/startup_movie_skipper/__init__.py
+++ src/py/startup_movie_skipper/__init__.py
@@ -1,16 +1,28 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING, cast
 from unrealsdk import find_all
 from unrealsdk.hooks import Type
-from mods_base import hook, build_mod
+from mods_base import hook, build_mod, Game, Library
+
+if TYPE_CHECKING:
+    from BL1E.WillowGame import WillowPlayerController, WillowGFxMoviePressStart
 
 
 @hook("Engine.GameViewportClient:Tick", Type.POST, immediately_enable=True)
-def hook_skip_to_main_menu(_1, _2, _3, _4) -> None:
+def hook_skip_to_main_menu(_1, _2, _3, _4) -> None:  # pyright: ignore[reportUnknownParameterType, reportMissingParameterType]
     for obj in find_all("WillowGame.WillowGFxMoviePressStart"):
         if obj.ObjectFlags & (0x400 | 0x200):
             continue
-        obj.extContinue()
+
+        menu = cast("WillowGFxMoviePressStart", obj)
+        if Game.get_current() == Game.BL1E:
+            wpc = cast("WillowPlayerController", menu.PlayerOwner)
+            menu.extContinue()
+            wpc.GFxUIManager.CloseWaitDialog()  # pyright: ignore[reportOptionalMemberAccess]
+        else:
+            menu.extContinue()
 
     hook_skip_to_main_menu.disable()
 
 
-build_mod()
+_ = build_mod(cls=Library)
